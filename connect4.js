5f66ae361abf6e164ce3068eff2ede98372aca82
jetsemrick <jet.semrick@gmail.com>
2021-05-01 15:57:52 -0500

    documentation

--- connect4.js
+++ connect4.js
@@ -1,12 +1,11 @@
-/*
-File Name: connect4.js
-Author: Jet Semrick
-Date: 04-28-2021
-Description: Connect 4.
-Needs:
---add animations
-*/
+/* ------------------------------------------
+* File Name: connect4.js
+* Author: Jet Semrick
+* Date: 05-01-2021
+* Description: Connect 4 using javascript, html, and css
+-------------------------------------------*/
 
+/* initialize turn to player 1 and create the gameboard */
 let turn = 1;
 let gameboard = [];
 for (let i = 0; i < 7; i++) 
@@ -14,6 +13,13 @@ for (let i = 0; i < 7; i++)
     gameboard.push([null,null,null,null,null,null,null]);
 }
 
+/*
+* @name    : run
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : manages game state and checks the win condition
+* @param   : cell i.e. area of game board clicked by user
+* @return  : none
+*/
 run = (cell) => {
     let marked = document.getElementById(cell).style.backgroundColor;
 
@@ -100,18 +106,46 @@ run = (cell) => {
     }
 }
 
+/*
+* @name    : selectRed
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : changes cell color to red
+* @param   : cell i.e. area of game board affected
+* @return  : none
+*/
 selectRed = (cell) => {
     document.getElementById(cell).style.backgroundColor = "red";
 }
 
+/*
+* @name    : selectYellow
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : changes cell color to yellow
+* @param   : cell i.e. area of game board affected
+* @return  : none
+*/
 selectYellow = (cell) => {
     document.getElementById(cell).style.backgroundColor = "yellow";
 }
 
+/*
+* @name    : selectBlue
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : changes cell color to blue
+* @param   : cell i.e. area of game board affected
+* @return  : none
+*/
 selectBlue = (cell) => {
     document.getElementById(cell).style.backgroundColor = "#5D5C61";
 }
 
+/*
+* @name    : getRow
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : iterates the find the lowest empty cell in the selected column
+* @param   : cell i.e. area of game board clicked by user
+* @return  : num cellRow i.e. row where piece will go
+*/
 getRow = (cell) => {
     cellRow = parseInt(cell.charAt(4));
 
@@ -125,11 +159,25 @@ getRow = (cell) => {
     return cellRow;
 }
 
+/*
+* @name    : getCol
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : parses the cell string to get column
+* @param   : cell i.e. area of game board clicked by user
+* @return  : num cellCol i.e. column where piece will go
+*/
 getCol = (cell) => {
     cellCol = parseInt(cell.charAt(10));
     return cellCol;
 }
 
+/*
+* @name    : checkHorizontalWin
+* @pre     : none
+* @post    : parses the gameboard for rows of 4 pieces in a horizontal row by the same player
+* @param   : none
+* @return  : bool, true if win detected, false else
+*/
 checkHorizontalWin = () => {
     for (let i = 0; i < 7; i++)
     {
@@ -148,6 +196,13 @@ checkHorizontalWin = () => {
     return false;
 }
 
+/*
+* @name    : checkVerticalWin
+* @pre     : none
+* @post    : parses the gameboard for cols of 4 pieces in a vertical row by the same player
+* @param   : none
+* @return  : bool, true if win detected, false else
+*/
 checkVerticalWin = () => {
     for (let i = 0; i < 4; i++)
     {
@@ -166,6 +221,13 @@ checkVerticalWin = () => {
     return false;
 }
 
+/*
+* @name    : checkRightDiagonalWin
+* @pre     : none
+* @post    : parses the gameboard for 4 pieces in a diagonal "/" by the same player
+* @param   : none
+* @return  : bool, true if win detected, false else
+*/
 checkRightDiagonalWin = () => {
     for (let i = 3; i < 7; i++)
     {
@@ -182,11 +244,17 @@ checkRightDiagonalWin = () => {
         }
     }
 }
-
+/*
+* @name    : checkLeftDiagonalWin
+* @pre     : none
+* @post    : parses the gameboard for 4 pieces in a diagonal "\" by the same player
+* @param   : none
+* @return  : bool, true if win detected, false else
+*/
 checkLeftDiagonalWin = () => {
     for (let i = 3; i < 7; i++)
     {
-        for (let j = 7; j > 3; j--)
+        for (let j = 7; j > 2; j--)
         {
             if (gameboard[i][j] == 0 && gameboard[i - 1][j - 1] == 0  && gameboard[i - 2][j - 2] == 0  && gameboard[i - 3][j - 3] == 0)
             {
@@ -200,6 +268,13 @@ checkLeftDiagonalWin = () => {
     }
 }
 
+/*
+* @name    : checkWin
+* @pre     : none
+* @post    : calls win condition checks
+* @param   : none
+* @return  : bool, true if win detected, false else
+*/
 checkWin = () => {
     if (checkHorizontalWin())
     {
@@ -217,8 +292,17 @@ checkWin = () => {
     {
         return true;
     }
+
+    return false;
 }
 
+/*
+* @name    : checkFull
+* @pre     : none
+* @post    : parses gameboard to check if the board is completely full i.e. unplayable
+* @param   : none
+* @return  : bool, true if board is full, false else
+*/
 checkFull = () => {
     let count = 0;
     for (let i = 0; i < 7; i++)
@@ -238,6 +322,13 @@ checkFull = () => {
     return false;
 }
 
+/*
+* @name    : animation
+* @pre     : cell must be in string format "row-0-col-0"
+* @post    : uses promises to "animate" the drop of the tokens
+* @param   : cell where token will land
+* @return  : none
+*/
 animation = (cell) => {
     let i = -1;
 
@@ -250,34 +341,28 @@ animation = (cell) => {
                 if (player == 0)
                 {
                     let promise = function delay() {
-                        console.log("Entered Function");
                         return new Promise((resolve, reject) => {
                             selectYellow("row-" + i.toString() + "-col-" + getCol(cell).toString());
                             setTimeout(() => {
-                                console.log("Inside Function");
                                 resolve("resolved");
                             }, 50);
                         });
                     }
                     promise().then(() => {
-                        console.log("Function received.");
                         selectBlue("row-" + i.toString() + "-col-" + getCol(cell).toString()); 
                     });
                 }
                 if (player == 1)
                 {
                     let promise = function delay() {
-                        console.log("Entered Function");
                         return new Promise((resolve, reject) => {
                             selectRed("row-" + i.toString() + "-col-" + getCol(cell).toString());
                             setTimeout(() => {
-                                console.log("Inside Function");
                                 resolve("resolved");
                             }, 50);
                         });
                     }
                     promise().then(() => {
-                        console.log("Function received.");
                         selectBlue("row-" + i.toString() + "-col-" + getCol(cell).toString()); 
                     });
                 }
